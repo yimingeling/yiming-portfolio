0a694055791c7c90447aa83686e915f287c3b751
yimingeling <yiming@live.nl>
2025-06-06 21:34:39 +0200

    changed browserrouter to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,34 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router";
-import PageNotFound from "./componenten/pages/pageNotFound.jsx";
-import Home from "./componenten/pages/home.jsx";
-import Layout from "./componenten/layout.jsx";
-import Contact from "./componenten/pages/contact.jsx";
-import Welcome from "./componenten/pages/welcome.jsx";
-import Projects from "./componenten/pages/projects.jsx";
+import {
+    HashRouter,
+    Routes,
+    Route
+} from 'react-router-dom'
 
-
-const router = createBrowserRouter([
-    {
-        element: <Layout/>,
-        children: [
-            {
-                path: '/',
-                element: <Welcome/>,
-            },
-            {
-                path: '/home',
-                element: <Home/>,
-            },
-
-            {
-                path: '/contact',
-                element: <Contact/>,
-            },
-            {
-                path: '/projects',
-                element: <Projects/>,
-            },
-            {
-                path: '*',
-                element: <PageNotFound/>,
-            },
-        ]
-    }
-]);
-
+import PageNotFound from "./componenten/pages/pageNotFound.jsx"
+import Home from "./componenten/pages/home.jsx"
+import Layout from "./componenten/layout.jsx"
+import Contact from "./componenten/pages/contact.jsx"
+import Welcome from "./componenten/pages/welcome.jsx"
+import Projects from "./componenten/pages/projects.jsx"
 
 function App() {
-    return <RouterProvider router={router}/>;
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<Welcome/>}/>
+                    <Route path="home" element={<Home/>}/>
+                    <Route path="contact" element={<Contact/>}/>
+                    <Route path="projects" element={<Projects/>}/>
+                    <Route path="*" element={<PageNotFound/>}/>
+                </Route>
+            </Routes>
+        </HashRouter>
+    );
 }
 
 export default App
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/yiming-portfolio',
+  base:'/yiming-portfolio/',
 })
