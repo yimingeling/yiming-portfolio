4f70965d98fa6dbcd478946d6e7589373c8ea23f
yimingeling <yiming@live.nl>
2025-09-05 17:21:13 +0200

    padding fixed for mobile

--- src/componenten/pages/contact.jsx
+++ src/componenten/pages/contact.jsx
@@ -4,12 +4,38 @@ import {Link} from "react-router";
 function Contact() {
     return (
         <>
-            <h1>
-                Contact
-            </h1>
-            <img id={'imgMe'}  src={'assets/me2.jpg'} alt={"photo of yiming li"}/>
+
+            <main className={'homeMain'}>
+                <header className="">
+                    <a className={'links '}>
+                        <Link to={'/home'}>
+                            ← back
+                        </Link>
+                    </a>
+                    <div className="">
+                        {/* Logo */}
+                        <Link to={'/'} className="logo">
+                            <img
+                                src="img/bird1.png"
+                                alt="Tetris logo"
+                                className="w-12 h-12"
+                            />
+                        </Link>
 
 
+                    </div>
+                </header>
+                <h1>
+                    Contact
+                </h1>
+                <img id={'imgMe'} src={'assets/me2.jpg'} alt={"photo of yiming li"}/>
+                <p>github:
+                    <a href="https://github.com/yimingeling"> https://github.com/yimingeling</a>
+                </p>
+                <p>
+                    email: yimingdoesdev@gmail.com
+                </p>
+            </main>
         </>
 
     )
--- src/componenten/pages/projects.jsx
+++ src/componenten/pages/projects.jsx
@@ -29,140 +29,42 @@ function Projects() {
 
                 <main className={'projectsMain'}>
 
+                    <article>
+                        <img src="img/commiebot.jpg" alt="picture of commiebot interface"/>
+                        <br/>
+
+                        <h1>commiebot</h1>
+
+                        <p> AI-chatbot demo showcase built with Azure OpenAI LLM using prompt engineering and
+                            embedding </p>
+                        <br/>
+                        <a href="https://github.com/yimingeling/schie-zo-friends-redux">link</a>
+
+                    </article>
 
                     <article>
-                        <h2>Project</h2>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
+                        <h1>schiezofriends redux</h1>
+                        the goal of this project is to clean/rewrite the code of the original hackaton game and
+                        implement/improve on missing features and bugfixes. <br/>
+                        <br/>
+                        <a href="https://github.com/yimingeling/schie-zo-friends-redux">link</a>
+
                     </article>
                     <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
-                    </article>
-                    <article>
-                        Project
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda pariatur recusandae repellat
-                        unde vel? Alias aliquam assumenda delectus dignissimos dolorem eligendi ex expedita inventore
-                        ipsam, iusto pariatur possimus quia quidem!
+                        <img src="img/schiezofriends.gif" alt="picture of schiezofriends game"/>
+                        <br/>
+                        <h1>schiezofriends</h1>
+                        2-player coop-game developed in excalibur.js for arcade machines in the university buildings
+                        <br/>
+                        <br/>
+                        <a href="https://github.com/surelynobody161/cle4">link</a>
+
                     </article>
 
+                    <article>
+                        <h1>yimingdoesdev</h1>
+                        <p> My personal portfolio website that you are looking at!</p>
+                    </article>
 
 
                 </main>
